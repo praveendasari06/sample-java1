612150f51bf707e028b04010332db9e11fabc668
praveendasari06 <praveen.dasari535@gmail.com>
2025-06-07 19:31:26 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2, "Basic addition should work");
+    }
+}
