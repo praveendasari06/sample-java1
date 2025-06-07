f6654e171dd5ee6339da9865fb8ab51ad8dbae38
praveendasari06 <praveen.dasari535@gmail.com>
2025-06-07 19:04:43 +0530

    Update AppTest.java

--- src/main/java/com/example/AppTest.java
+++ src/main/java/com/example/AppTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+
     @Test
-    public void testApp() {
-        assertEquals(2, 1 + 1);
+    void testSum() {
+        int result = 2 + 3;
+        assertEquals(5, result, "Sum should be 5");
     }
 }
